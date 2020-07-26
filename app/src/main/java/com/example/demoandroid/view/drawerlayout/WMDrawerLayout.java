3906f04ada8ffdce0bf2264f4b7030551683bd36
zhanghongjun <zhanghongjun163@228.com>
2020-07-26 15:45:06 +0800

    添加滑动

--- app/src/main/java/com/example/demoandroid/view/drawerlayout/WMDrawerLayout.java
+++ app/src/main/java/com/example/demoandroid/view/drawerlayout/WMDrawerLayout.java
@@ -1,9 +1,13 @@
 package com.example.demoandroid.view.drawerlayout;
 
 import android.content.Context;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
@@ -15,8 +19,11 @@ public class WMDrawerLayout extends FrameLayout {
     private View leftMenu;
     private View mainView;
 
-    private float percent = 0.7f;
-    private int sloup;
+    private float percent;
+    private int touchSlop;
+
+    private int mLastX;
+    private int mLastY;
 
     public WMDrawerLayout(@NonNull Context context) {
         super(context);
@@ -29,6 +36,9 @@ public class WMDrawerLayout extends FrameLayout {
     }
 
     private void initView(Context context, AttributeSet attrs) {
+        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        percent = 0.7f;
+
         leftMenu = LayoutInflater.from(context).inflate(R.layout.left_menu_layout, null);
         mainView = LayoutInflater.from(context).inflate(R.layout.middle_menu_layout, null);
 
@@ -73,5 +83,82 @@ public class WMDrawerLayout extends FrameLayout {
     }
 
 
+    //测试是一个什么样的事件
+    private boolean isTestComplete;
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        if (!isTestComplete) {
+            getEventType(ev);
+            return true;
+        }
+
+        if (isHorizontalScroll) {
+            switch (ev.getAction()) {
+                case MotionEvent.ACTION_MOVE:
+                    int curScrollX = getScrollX();
+                    int disX = (int) (ev.getX() - point.x);
+                    int expectX = -disX + curScrollX;
+                    int finalX = 0;
+                    if (expectX < 0) {//向左滑动
+                        finalX = Math.max(expectX, -leftMenu.getMeasuredWidth());
+                    } else {//向右滑动
+
+                    }
+
+                    scrollTo(finalX, 0);
+                    point.x = (int) ev.getX();
+                    point.y = (int) ev.getY();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    isHorizontalScroll = false;
+                    isTestComplete = false;
+                    break;
+            }
+        }else {
+            switch (ev.getAction()){
+                case MotionEvent.ACTION_UP:
+                    isHorizontalScroll = false;
+                    isTestComplete = false;
+                    break;
+            }
+        }
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private Point point = new Point();
+    private boolean isHorizontalScroll; //是否是水平滑块
+
+    private void getEventType(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                point.x = (int) ev.getX();
+                point.y = (int) ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dx = (int) Math.abs(ev.getX() - point.x);
+                int dy = (int) Math.abs(ev.getY() - point.y);
+                if (dx > touchSlop && dx > dy) { // 水平滑动
+                    isHorizontalScroll = true;
+                    point.x = (int) ev.getX();
+                    point.y = (int) ev.getY();
+                    isTestComplete = true;
+                } else if (dy > touchSlop && dy > dx) { //垂直滑块
+                    isHorizontalScroll = false;
+                    point.x = (int) ev.getX();
+                    point.y = (int) ev.getY();
+                    isTestComplete = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                break;
+
+        }
+
+
+    }
 }
